ebe861e7cbf1f4ffc7919c2a6e7da9e1b8010c9e
aqshol afid <aqsholafid2@gmail.com>
2022-01-22 22:07:40 +0700

    finish dummy data

--- pages/video.tsx
+++ pages/video.tsx
@@ -1,15 +1,19 @@
 import Layout from "components/Layout";
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import ReactPlayer from "react-player/lazy";
-import { Show, ChevronDown,Send, Heart, MoreCircle } from "react-iconly";
+import { Show, ChevronDown, Send, Heart, MoreCircle } from "react-iconly";
 import Image from "next/image";
+import clsx from 'clsx';
 
 interface Props {}
 
 export default function Video({}: Props): ReactElement {
+  const [showDesc, setshowDesc] = useState<boolean>(false);
   return (
     <Layout>
-      
+      {/* 
+          Video Player
+      */}
       <div className="w-full h-[261px] flex justify-center px-6 rounded-xl overflow-hidden relative">
         <ReactPlayer
           width={"100%"}
@@ -19,13 +23,20 @@ export default function Video({}: Props): ReactElement {
         />
         <div id="control-video"></div>
       </div>
+
+      {/* 
+          Video Desc
+      */}
       <div className="pb-6 px-6 mt-6 text-white">
         <div className="flex items-start justify-between">
           <h1 className="text-xl w-72">
             How to ride your skateboard and Basic Equipment
           </h1>
-          <button className="w-max flex justify-center items-center bg-white bg-opacity-10 rounded-full p-1 ">
-            <ChevronDown primaryColor="white" size={"small"} />
+          <button
+            className={clsx("w-max flex justify-center items-center bg-white bg-opacity-10 rounded-full p-1 transition-all transform ", showDesc && ["rotate-180"])}
+            onClick={() => setshowDesc(!showDesc)}
+          >
+            <ChevronDown primaryColor="white" size={"small"}  />
           </button>
         </div>
 
@@ -40,6 +51,21 @@ export default function Video({}: Props): ReactElement {
             <p className="text-xs text-[#808191]">47,987 likes</p>
           </div>
         </div>
+        {showDesc && <p className="text-xs font-extralight leading-4 tracking-wide mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          soluta laboriosam nulla provident modi recusandae quod obcaecati iste
+          unde officiis minus rerum fuga illum perferendis, dolorum, iusto ad
+          quis quidem quisquam! Odio excepturi reprehenderit repellendus
+          possimus, mollitia laboriosam consequatur laborum hic tenetur rem sint
+          adipisci cumque ducimus. Sunt distinctio facilis laborum, saepe dicta
+          recusandae officiis repellat beatae dignissimos cupiditate dolores,
+          illum accusantium delectus, nesciunt neque voluptates. Sed quasi
+          consequatur consectetur fugiat temporibus! Voluptas ullam temporibus
+          ipsam, eveniet quos magni perferendis qui, veritatis natus nemo
+          quidem, vel itaque aut quas commodi accusamus assumenda dicta?
+          Accusantium quos enim eum asperiores placeat quasi!
+        </p>}
+        
       </div>
 
       <div className="pl-6">
@@ -75,13 +101,21 @@ export default function Video({}: Props): ReactElement {
         </div>
       </div>
 
-      <div className="px-6 absolute bottom-10 left-0 right-0 mt-auto">
+      <div className="px-6 absolute bottom-0 mb-2 left-0 right-0 mt-auto">
         <div className="p-2 box-border mt-8  rounded-full bg-[#252836] flex items-center">
           <div id="icon-more">
             <MoreCircle filled primaryColor="#666872" />
           </div>
-          <input type="text" name="" id="" placeholder="Write your message" className="text-xs text-white bg-transparent placeholder:text-xs placeholder:text-[#808191] basis-full min-w-0 ml-3" />
-          <button className="p-2 rounded-full bg-[#6C5ECF] justify-center items-center"><Send size={"small"} filled primaryColor="white"/></button>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Write your message"
+            className="text-xs text-white bg-transparent placeholder:text-xs placeholder:text-[#808191] basis-full min-w-0 ml-3"
+          />
+          <button className="p-2 rounded-full bg-[#6C5ECF] justify-center items-center">
+            <Send size={"small"} filled primaryColor="white" />
+          </button>
         </div>
       </div>
     </Layout>
