c7e48d971e992088865cf0d03448cf69e086a7d5
aqshol afid <aqsholafid2@gmail.com>
2022-01-23 10:40:31 +0700

    fixing layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,20 +11,23 @@ import Stories from "components/section/Stories";
 export default function Home() {
   return (
     <Layout>
-      <Stories />
-      <div className="grid grid-cols-12  min-h-screen  relative items-start ">
-        <div className="col-span-2 lg:block hidden sticky top-0  pl-6 lg:px-6">
-          <SideBar />
-        </div>
-        <div className="col-span-12 mt-6 lg:mt-0 lg:col-start-3 lg:col-span-10 pl-6 lg:px-6">
-          <DiscoverSection />
-
-          <MostWatchedSection />
-        </div>
-        <div className="col-span-12 fixed w-full bottom-0">
-          <BottomNav />
-        </div>
+      <div className="col-span-12">
+        <Stories />
       </div>
+      
+      <div className="col-span-2 lg:block hidden sticky  top-0  pl-6 lg:px-6">
+        <SideBar />
+      </div>
+      <div className="col-span-12 mt-6 lg:mt-0 lg:col-start-3 lg:col-span-10 pl-6 lg:px-6">
+        <DiscoverSection />
+
+        <MostWatchedSection />
+        
+      </div>
+      <div className="col-span-12 fixed w-full bottom-0">
+        <BottomNav />
+      </div>
+      
     </Layout>
   );
 }
--- pages/video.tsx
+++ pages/video.tsx
@@ -1,9 +1,9 @@
 import Layout from "components/Layout";
 import React, { ReactElement, useState } from "react";
-import ReactPlayer from "react-player/lazy";
+import ReactPlayer from "react-player/youtube";
 import { Show, ChevronDown, Send, Heart, MoreCircle } from "react-iconly";
 import Image from "next/image";
-import clsx from 'clsx';
+import clsx from "clsx";
 
 interface Props {}
 
@@ -11,111 +11,118 @@ export default function Video({}: Props): ReactElement {
   const [showDesc, setshowDesc] = useState<boolean>(false);
   return (
     <Layout>
-      {/* 
+      <div className="col-span-12 relative min-h-screen">
+        {/* 
           Video Player
       */}
-      <div className="w-full h-[261px] flex justify-center px-6 rounded-xl overflow-hidden relative">
-        <ReactPlayer
-          width={"100%"}
-          height={"100%"}
-          url="https://youtu.be/h9wualcJuE4"
-          controls={false}
-        />
-        <div id="control-video"></div>
-      </div>
+        <div className="w-full h-[261px] lg:h-[500px]  flex justify-center px-6 rounded-3xl overflow-hidden relative">
+          <ReactPlayer
+            width={"100%"}
+            height={"100%"}
+            url="https://youtu.be/h9wualcJuE4"
+            controls={false}
+          />
+          <div id="control-video"></div>
+        </div>
 
-      {/* 
+        {/* 
           Video Desc
       */}
-      <div className="pb-6 px-6 mt-6 text-white">
-        <div className="flex items-start justify-between">
-          <h1 className="text-xl w-72">
-            How to ride your skateboard and Basic Equipment
-          </h1>
-          <button
-            className={clsx("w-max flex justify-center items-center bg-white bg-opacity-10 rounded-full p-1 transition-all transform ", showDesc && ["rotate-180"])}
-            onClick={() => setshowDesc(!showDesc)}
-          >
-            <ChevronDown primaryColor="white" size={"small"}  />
-          </button>
-        </div>
-
-        <div className="mt-4 flex gap-6">
-          <div id="views" className="flex gap-1 items-center">
-            <Show primaryColor="#808191" size={"small"} />
-
-            <p className="text-[#808191] text-xs">125.908 views</p>
+        <div className="pb-6 px-6 mt-6 text-white">
+          <div className="flex items-start justify-between">
+            <h1 className="text-xl lg:text-2xl w-72 lg:w-full">
+              How to ride your skateboard and Basic Equipment
+            </h1>
+            <button
+              className={clsx(
+                "w-max flex justify-center items-center bg-white bg-opacity-10 rounded-full p-1 transition-all transform ",
+                showDesc && ["rotate-180"]
+              )}
+              onClick={() => setshowDesc(!showDesc)}
+            >
+              <ChevronDown primaryColor="white" size={"small"} />
+            </button>
           </div>
-          <div id="likes" className="flex items-center gap-1">
-            <Heart primaryColor="#808191" size={"small"} />
-            <p className="text-xs text-[#808191]">47,987 likes</p>
-          </div>
-        </div>
-        {showDesc && <p className="text-xs font-extralight leading-4 tracking-wide mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-          soluta laboriosam nulla provident modi recusandae quod obcaecati iste
-          unde officiis minus rerum fuga illum perferendis, dolorum, iusto ad
-          quis quidem quisquam! Odio excepturi reprehenderit repellendus
-          possimus, mollitia laboriosam consequatur laborum hic tenetur rem sint
-          adipisci cumque ducimus. Sunt distinctio facilis laborum, saepe dicta
-          recusandae officiis repellat beatae dignissimos cupiditate dolores,
-          illum accusantium delectus, nesciunt neque voluptates. Sed quasi
-          consequatur consectetur fugiat temporibus! Voluptas ullam temporibus
-          ipsam, eveniet quos magni perferendis qui, veritatis natus nemo
-          quidem, vel itaque aut quas commodi accusamus assumenda dicta?
-          Accusantium quos enim eum asperiores placeat quasi!
-        </p>}
-        
-      </div>
 
-      <div className="pl-6">
-        <hr className="opacity-5" />
-      </div>
-      <div className="px-6 mt-6">
-        <div className="flex gap-2">
-          <div
-            id="comment-face"
-            className="basis-12 relative flex-shrink-0 h-12 rounded-full bg-gray-400 overflow-hidden"
-          >
-            <Image
-              src="/image/profile/image 13.png"
-              layout="fill"
-              objectFit="cover"
-              alt="comment profile"
-              objectPosition={"center"}
-            />
+          <div className="mt-4 flex gap-6">
+            <div id="views" className="flex gap-1 items-center">
+              <Show primaryColor="#808191" size={"small"} />
+
+              <p className="text-[#808191] text-xs">125.908 views</p>
+            </div>
+            <div id="likes" className="flex items-center gap-1">
+              <Heart primaryColor="#808191" size={"small"} />
+              <p className="text-xs text-[#808191]">47,987 likes</p>
+            </div>
           </div>
-          <div
-            id="comment-detail"
-            className="p-4 rounded-bl-xl rounded-r-xl bg-[#252836]"
-          >
-            <h2 className="text-sm text-white">
-              Wijaya Abadi{" "}
-              <span className="bg-[#22B07D] inline-block w-2 ml-1 h-2 rounded-full "></span>
-            </h2>
-            <p className="text-xs text-[#808191] mt-2 leading-5">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
-              harum?
+          {showDesc && (
+            <p className="text-xs font-extralight leading-4 tracking-wide mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              soluta laboriosam nulla provident modi recusandae quod obcaecati
+              iste unde officiis minus rerum fuga illum perferendis, dolorum,
+              iusto ad quis quidem quisquam! Odio excepturi reprehenderit
+              repellendus possimus, mollitia laboriosam consequatur laborum hic
+              tenetur rem sint adipisci cumque ducimus. Sunt distinctio facilis
+              laborum, saepe dicta recusandae officiis repellat beatae
+              dignissimos cupiditate dolores, illum accusantium delectus,
+              nesciunt neque voluptates. Sed quasi consequatur consectetur
+              fugiat temporibus! Voluptas ullam temporibus ipsam, eveniet quos
+              magni perferendis qui, veritatis natus nemo quidem, vel itaque aut
+              quas commodi accusamus assumenda dicta? Accusantium quos enim eum
+              asperiores placeat quasi!
             </p>
+          )}
+        </div>
+
+        <div className="pl-6">
+          <hr className="opacity-5" />
+        </div>
+        <div className="px-6 mt-6">
+          <div className="flex gap-2">
+            <div
+              id="comment-face"
+              className="basis-12 relative flex-shrink-0 h-12 rounded-full bg-gray-400 overflow-hidden"
+            >
+              <Image
+                src="/image/person/image 12.png"
+                layout="fill"
+                objectFit="cover"
+                alt="comment profile"
+                objectPosition={"center"}
+              />
+            </div>
+            <div
+              id="comment-detail"
+              className="p-4 rounded-bl-xl rounded-r-xl bg-[#252836]"
+            >
+              <h2 className="text-sm text-white">
+                Wijaya Abadi{" "}
+                <span className="bg-[#22B07D] inline-block w-2 ml-1 h-2 rounded-full "></span>
+              </h2>
+              <p className="text-xs text-[#808191] mt-2 leading-5">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
+                harum?
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="px-6 absolute bottom-0 mb-2 left-0 right-0 mt-auto">
-        <div className="p-2 box-border mt-8  rounded-full bg-[#252836] flex items-center">
-          <div id="icon-more">
-            <MoreCircle filled primaryColor="#666872" />
+        <div className="fixed bottom-10 px-6 mb-2 w-full max-w-screen-2xl">
+          <div className="p-2 box-border mt-8  rounded-full bg-[#252836] flex items-center">
+            <div id="icon-more">
+              <MoreCircle filled primaryColor="#666872" />
+            </div>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Write your message"
+              className="text-xs text-white bg-transparent placeholder:text-xs placeholder:text-[#808191] basis-full min-w-0 ml-3"
+            />
+            <button className="p-2 rounded-full bg-[#6C5ECF] justify-center items-center">
+              <Send size={"small"} filled primaryColor="white" />
+            </button>
           </div>
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Write your message"
-            className="text-xs text-white bg-transparent placeholder:text-xs placeholder:text-[#808191] basis-full min-w-0 ml-3"
-          />
-          <button className="p-2 rounded-full bg-[#6C5ECF] justify-center items-center">
-            <Send size={"small"} filled primaryColor="white" />
-          </button>
         </div>
       </div>
     </Layout>
