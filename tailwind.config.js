8f717c406c8022c1599273168987fde6fd9b0b6f
aqshol afid <aqsholafid2@gmail.com>
2022-01-29 20:34:25 +0700

    fix bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  // darkMode:'class',
   theme: {
     fontFamily: {
       poppins: ['"Poppins"', "sans-serif"],
